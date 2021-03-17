3402ca2512bf61cd873becfd3014299887a5a2a3
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-03-17 10:00:42 +0100

    fiks path for svg

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -38,6 +38,9 @@ module.exports = {
         use: [
           {
             loader: "file-loader",
+            options: {
+              publicPath: "/",
+            },
           },
         ],
       },
