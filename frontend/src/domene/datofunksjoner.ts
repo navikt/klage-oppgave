8c3aca1209c16dbdd01bd5c09de289e74ebed3fb
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-05-10 08:46:53 +0200

    ta høyde for at dato kan være NaN

--- frontend/src/domene/datofunksjoner.ts
+++ frontend/src/domene/datofunksjoner.ts
@@ -1,7 +1,9 @@
 export const formattedDate = (frist: number) => {
   const utime = new Date(frist).getTime();
-  const ye = new Intl.DateTimeFormat("nb", { year: "numeric" }).format(utime);
-  const mo = new Intl.DateTimeFormat("nb", { month: "2-digit" }).format(utime);
-  const da = new Intl.DateTimeFormat("nb", { day: "2-digit" }).format(utime);
-  return `${da}${mo}${ye}`;
+  if (!isNaN(utime)) {
+    const ye = new Intl.DateTimeFormat("nb", { year: "numeric" }).format(utime);
+    const mo = new Intl.DateTimeFormat("nb", { month: "2-digit" }).format(utime);
+    const da = new Intl.DateTimeFormat("nb", { day: "2-digit" }).format(utime);
+    return `${da}${mo}${ye}`;
+  } else return "mangler";
 };
