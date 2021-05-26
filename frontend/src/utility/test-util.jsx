dcfb480d6603b29a553bef07cdca787252d7bc3b
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-05-26 12:46:09 +0200

    legg til cursor på etikett

--- frontend/src/utility/test-util.jsx
+++ frontend/src/utility/test-util.jsx
@@ -1,9 +1,7 @@
-// test-utils.jsx
 import React from "react";
 import { render as rtlRender } from "@testing-library/react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-// Import your own reducer
 import reducer from "../tilstand/root";
 
 function render(
