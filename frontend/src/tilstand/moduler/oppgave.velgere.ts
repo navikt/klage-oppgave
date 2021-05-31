e1efb8d6394e2f788517b1bc7300ba452f9865c7
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-05-31 14:27:18 +0200

    revert endring av rootstate, det får være en egen fiks

--- frontend/src/tilstand/moduler/oppgave.velgere.ts
+++ frontend/src/tilstand/moduler/oppgave.velgere.ts
@@ -1,24 +1,27 @@
+import { RootStateOrAny } from "react-redux";
 import { OppgaveRader } from "./oppgave";
-import { RootState } from "../root";
 
-export function velgOppgaver(state: RootState) {
-  return state.klagebehandlinger;
+// The function below is called a selector and allows us to select a value
+// from the state. These are kept in a separate file from the rest of the
+// Redux module to avoid circular dependencies.
+export function velgOppgaver(state: RootStateOrAny) {
+  return state.klagebehandlinger as OppgaveRader;
 }
-export function velgFerdigstilteOppgaver(state: RootState) {
-  return state.klagebehandlinger.ferdigstilteKlager;
+export function velgFerdigstilteOppgaver(state: RootStateOrAny) {
+  return state.klagebehandlinger.ferdigstilteKlager as OppgaveRader;
 }
-export function velgSideLaster(state: RootState) {
-  return state.klagebehandlinger.lasterData;
+export function velgSideLaster(state: RootStateOrAny) {
+  return state.klagebehandlinger.lasterData as boolean;
 }
-export function velgFiltrering(state: RootState) {
+export function velgFiltrering(state: RootStateOrAny) {
   return state.klagebehandlinger.transformasjoner.filtrering;
 }
-export function velgSortering(state: RootState) {
+export function velgSortering(state: RootStateOrAny) {
   return state.klagebehandlinger.transformasjoner.sortering;
 }
-export function velgProjeksjon(state: RootState) {
+export function velgProjeksjon(state: RootStateOrAny) {
   return state.klagebehandlinger.meta.projeksjon;
 }
-export function velgKodeverk(state: RootState) {
+export function velgKodeverk(state: RootStateOrAny) {
   return state.klagebehandlinger.kodeverk;
 }
