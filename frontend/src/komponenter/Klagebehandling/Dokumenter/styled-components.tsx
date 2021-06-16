19ecf144427c8339858890cf7612176e3bca0cf3
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-06-16 16:13:24 +0200

    Legg tilbake endring i overflow

--- frontend/src/komponenter/Klagebehandling/Dokumenter/styled-components.tsx
+++ frontend/src/komponenter/Klagebehandling/Dokumenter/styled-components.tsx
@@ -6,6 +6,7 @@ export const DokumenterBeholder = styled.section<{ fullvisning: boolean }>`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 4px;
   position: relative;
+  overflow: scroll;
   min-width: ${({ fullvisning }) => (fullvisning ? "40em" : "16em")};
   height: 100%;
 `;
