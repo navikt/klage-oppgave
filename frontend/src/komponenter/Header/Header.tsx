f939cdf8fb173fdb8d6d35e7f82100b56ad7705c
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-06-25 10:01:50 +0200

    fix: viser enheter for innloggede saksbehandlere

--- frontend/src/komponenter/Header/Header.tsx
+++ frontend/src/komponenter/Header/Header.tsx
@@ -84,25 +84,22 @@ export const Bruker = ({ navn, ident, enhet, rolle }: Brukerinfo) => {
       </button>
       <div className={classNames(aapen ? "velg-enhet maksimert" : "minimert")} ref={ref}>
         <div className={"enheter"}>
-          {(() => {
-            if (person.enheter) {
-              enheter.map((enhet, index) => {
-                return (
-                  <div
-                    className={classNames({
-                      enhet: true,
-                      active: person.enheter[person.valgtEnhet].id === enhet.id,
-                    })}
-                    key={enhet.id}
-                  >
-                    <NavLink to={"#"} onClick={(e) => settEnhet(e, index)}>
-                      {enhet.id} {enhet.navn}
-                    </NavLink>
-                  </div>
-                );
-              });
-            }
-          })()}
+          {!harAdminTilgang &&
+            enheter.map((enhet, index) => {
+              return (
+                <div
+                  className={classNames({
+                    enhet: true,
+                    active: person.enheter[person.valgtEnhet].id === enhet.id,
+                  })}
+                  key={enhet.id}
+                >
+                  <NavLink to={"#"} onClick={(e) => settEnhet(e, index)}>
+                    {enhet.id} {enhet.navn}
+                  </NavLink>
+                </div>
+              );
+            })}
           <hr />
           {harAdminTilgang && (
             <NavLink to={"/admin"} className={classNames({ enhet: true, navlink: true })}>
